f75c0884c4aca5ac9924f6e5cd978f0b8c8047a6
anisahdenis <anisahdenis.1595@gmail.com>
2020-11-04 09:11:42 +0700

    update perbaikan list dan form login

--- application/views/templates/footer-1.php
+++ application/views/templates/footer-1.php
@@ -1,61 +1,61 @@
 	<!--==================== FOOTER ====================-->
-	 <!-- footer -->
-            <!-- ============================================================== -->
-            <footer class="footer">
-                © 2020 Kominfo Kota Pasuruan
-            </footer>
-            <!-- ============================================================== -->
-            <!-- End footer -->
-            <!-- ============================================================== -->
-	<!--================== FOOTER END ==================-->
+  <!-- footer -->
+  <!-- ============================================================== -->
+  <footer class="footer">
+    © 2020 Kominfo Kota Pasuruan
+</footer>
+<!-- ============================================================== -->
+<!-- End footer -->
+<!-- ============================================================== -->
+<!--================== FOOTER END ==================-->
 
-	<!--===================== TO TOP =====================-->
-	<a class="to-top" href="#home">
-		<i class="fa fa-angle-double-up" aria-hidden="true"></i>
-	</a>
-	<!--=================== TO TOP END ===================-->
-	<!--==================== SCRIPT	====================-->
-	 <script src="<?=base_url()?>_assets/material/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="<?=base_url()?>_assets/material/plugins/popper/popper.min.js"></script>
-    <script src="<?=base_url()?>_assets/material/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="<?=base_url()?>_assets/material/js/jquery.slimscroll.js"></script>
-    <!--Wave Effects -->
-    <script src="<?=base_url()?>_assets/material/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="<?=base_url()?>_assets/material/js/sidebarmenu.js"></script>
-    <!--stickey kit -->
-    <script src="<?=base_url()?>_assets/material/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
-    <script src="<?=base_url()?>_assets/material/plugins/sparkline/jquery.sparkline.min.js"></script>
-    <!--Custom JavaScript -->
-    <script src="<?=base_url()?>_assets/material/js/custom.min.js"></script>
-    <!-- ============================================================== -->
-    <!-- This page plugins -->
-    <!-- ============================================================== -->
-   
-    <!--c3 JavaScript -->
-    <script src="<?=base_url()?>_assets/material/plugins/d3/d3.min.js"></script>
-    <script src="<?=base_url()?>_assets/material/plugins/c3-master/c3.min.js"></script>
-    <!-- Chart JS -->
-    <script src="<?=base_url()?>_assets/material/js/dashboard1.js"></script>
-    <!-- ============================================================== -->
-    <!-- Style switcher -->
-    <!-- ============================================================== -->
-    <script src="<?=base_url()?>_assets/material/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+<!--===================== TO TOP =====================-->
+<a class="to-top" href="#home">
+  <i class="fa fa-angle-double-up" aria-hidden="true"></i>
+</a>
+<!--=================== TO TOP END ===================-->
+<!--==================== SCRIPT	====================-->
+<script src="<?=base_url()?>_assets/material/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="<?=base_url()?>_assets/material/plugins/popper/popper.min.js"></script>
+<script src="<?=base_url()?>_assets/material/plugins/bootstrap/js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="<?=base_url()?>_assets/material/js/jquery.slimscroll.js"></script>
+<!--Wave Effects -->
+<script src="<?=base_url()?>_assets/material/js/waves.js"></script>
+<!--Menu sidebar -->
+<script src="<?=base_url()?>_assets/material/js/sidebarmenu.js"></script>
+<!--stickey kit -->
+<script src="<?=base_url()?>_assets/material/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+<script src="<?=base_url()?>_assets/material/plugins/sparkline/jquery.sparkline.min.js"></script>
+<!--Custom JavaScript -->
+<script src="<?=base_url()?>_assets/material/js/custom.min.js"></script>
+<!-- ============================================================== -->
+<!-- This page plugins -->
+<!-- ============================================================== -->
 
-	<!-- Custom Footer Start -->
+<!--c3 JavaScript -->
+<script src="<?=base_url()?>_assets/material/plugins/d3/d3.min.js"></script>
+<script src="<?=base_url()?>_assets/material/plugins/c3-master/c3.min.js"></script>
+<!-- Chart JS -->
+<script src="<?=base_url()?>_assets/material/js/dashboard1.js"></script>
+<!-- ============================================================== -->
+<!-- Style switcher -->
+<!-- ============================================================== -->
+<script src="<?=base_url()?>_assets/material/plugins/styleswitcher/jQuery.style.switcher.js"></script>
 
-	 </div>
-        <!-- ============================================================== -->
-        <!-- End Page wrapper  -->
-        <!-- ============================================================== -->
-    </div>
-    <!-- ============================================================== -->
-    <!-- End Wrapper -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
-   
+<!-- Custom Footer Start -->
+
+</div>
+<!-- ============================================================== -->
+<!-- End Page wrapper  -->
+<!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- End Wrapper -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+
 </body>
